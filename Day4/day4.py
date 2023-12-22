6de028f2fe849e163ca9510edd098f7dd7699fcf
Andrés <nunezlopez.ja@gmail.com>
2023-12-22 15:50:55 -0600

    some clean progress on day4

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 
+import re
 from sys import argv
 
 
-def main():
-    print("Hello")
+def get_checksum(line: str) -> str:
+    return re.findall(r'(\[[a-z]+\])', line)[0].strip("[]")
 
+
+def get_id(line: str) -> int:
+    return int(re.findall(r'([0-9]+)', line)[0])
+
+
+def get_letters(line: str) -> list:
+    letters = []
+    for i in line:
+        if i != '-' and i.isalpha:
+            letters.append(i)
+    return letters
+
+
+def main():
     if len(argv) < 2:
         exit("Usage: python day4.py input")
 
     with open(argv[1], 'r') as fp:
-        names = fp.strip().readlines()
+        names = [name.strip() for name in fp.readlines()]
 
-    print(names)
-
+    for name in names:
+        print(get_letters(name), get_id(name), get_checksum(name))
 
 if __name__ == "__main__":
     main()
