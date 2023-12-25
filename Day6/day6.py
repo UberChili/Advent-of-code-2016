ee210287188602c4ffa5e21a04cbb665bb92b604
Andrés <nunezlopez.ja@gmail.com>
2023-12-25 13:59:43 -0600

    Finished Day6 part 1. Surprisingly easy and got correct answer on first try, although it seemed weird

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
 from sys import argv
 
 
@@ -7,12 +8,20 @@ def main():
     if len(argv) < 2:
         exit("Usage: python day6.py file")
 
-    columns = {}
     with open(argv[1], 'r') as fp:
         lines = fp.readlines()
 
-        for line in lines:
-            print(line.strip())
+    columns = defaultdict(list)
+    for line in lines:
+        for i in range(len(line.strip())):
+            columns[i].append(line[i])
+
+    message = ""
+    for i in columns:
+        # print(max(columns[i], key=columns[i].count))
+        message += max(columns[i], key=columns[i].count)
+
+    print(message)
 
 
 if __name__ == "__main__":
