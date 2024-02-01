7a8ae03e414a871a370d41ce634e360fa0ab1f34
Andrés <nunezlopez.ja@gmail.com>
2024-02-01 17:10:56 -0600

    got to something

--- day9/day9.py
+++ day9/day9.py
@@ -17,16 +17,31 @@ def main():
 
 
 def catch(s):
-    pattern = r"((\d)x(\d))"
+    # pattern = r"((\d+)x(\d+))"
+    # pattern = r"(\d+x\d+)"
+    pattern = r"\(\d+x\d+\)"
 
-    if m := re.search(pattern, s):
-        length, times = [int(x) for x in m.group(1).split('x')]
-        # print(m.group(0))
-        n_s = s.replace(f"({length}x{times})", '', 1)
-        print(n_s)
+    if m := re.finditer(pattern, s):
+        # print(m.group(0).strip('()').split('x'))
+        for match in m:
+            # print(match.group(0))
+            start_pos = match.start()
+            end_pos = match.end()
+            length, times = match.group(0).strip('()').split('x')
+            chunk_to_delete = s[:start_pos] + s[end_pos:]
+            # chunk_to_add = chunk_to_delete + s[end_pos + int(length) - 1]
+            chunk_to_add = s[end_pos : end_pos + int(length)]
+            print(chunk_to_add)
+            # print(chunk)
+            # s = s.replace(matech.group(0), '', 1)
+        # current = m.group(0)
+        # length, times = current.strip('()').split('x')
+        # print(length, times)
+        # s = s.replace(f"({length}x{times})", '', 1)
+        # s = s.replace(current, '', 1)
     else:
-        print("pattern not found")
-
+        print("no pattern foudn")
+        # print("pattern not found")
 
 
 if __name__ == "__main__":
